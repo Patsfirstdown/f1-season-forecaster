73e6976035a8d9a1782f2714817ab3cb2ab7add2
Patsfirstdown <43241638+Patsfirstdown@users.noreply.github.com>
2026-06-14 22:25:42 -0500

    Update wcc.js

--- Dashboard/wcc.js
+++ Dashboard/wcc.js
@@ -30,6 +30,7 @@ function populateRaceDropdown() {
 
         raceSelect.appendChild(option);
     });
+    raceSelect.selectedIndex = raceSelect.options.length - 1;
 }
 
 function populateTeamDropdown(raceName) {
