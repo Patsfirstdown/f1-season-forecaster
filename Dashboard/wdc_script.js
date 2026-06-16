60a8230cd533c8c2ac5695f4b00ff19c5c88521d
Patsfirstdown <43241638+Patsfirstdown@users.noreply.github.com>
2026-06-16 14:19:15 -0500

    Update wdc_script.js

--- Dashboard/wdc_script.js
+++ Dashboard/wdc_script.js
@@ -130,7 +130,7 @@ function updateScore(selectedRace) {
 
     Object.keys(wdcData).forEach(driver => {
 
-        wdcList.push({
+        scoreList.push({
             driver: driver,
             probability: wdcData[driver].driver_score
         });
@@ -180,7 +180,7 @@ function updateWDCHeatmap(selectedRace) {
     let html = `
         <table class="heatmap-table">
             <tr>
-                <th class="heatmap-header>Driver</th>
+                <th class="heatmap-header">Driver</th>
     `;
 
     for (let pos = 1; pos <= 22; pos++) {
