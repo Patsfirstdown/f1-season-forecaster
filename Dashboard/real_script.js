8c01a9e0c8cc6a153589ce3a3b3118b312a6a44c
Patsfirstdown <43241638+Patsfirstdown@users.noreply.github.com>
2026-06-26 23:00:37 -0500

    Update real_script.js

--- Dashboard/real_script.js
+++ Dashboard/real_script.js
@@ -12,9 +12,9 @@ function interpolateColor(score) {
     const start = { r: 232, g: 0, b: 45 };
     const end = { r: 255, g: 255, b: 255 };
 
-    const r = Math.round(start.r + (end.r - start.r) * score);
-    const g = Math.round(start.g + (end.g - start.g) * score);
-    const b = Math.round(start.b + (end.b - start.b) * score);
+    const r = Math.round(start.r + (end.r - start.r) * (1-score));
+    const g = Math.round(start.g + (end.g - start.g) * (1-score));
+    const b = Math.round(start.b + (end.b - start.b) * (1-score));
 
     return `rgb(${r}, ${g}, ${b})`;
 }
