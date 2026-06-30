2f79d8cc5f8cf291a9e8c726df8db5e3cfe3a974
Patsfirstdown <43241638+Patsfirstdown@users.noreply.github.com>
2026-06-30 16:52:45 -0500

    Update dashboard.js

--- Dashboard/dashboard.js
+++ Dashboard/dashboard.js
@@ -830,7 +830,7 @@ function updateUpdates() {
         oldtempWCC1=currenttempWCC1;
         oldtempWDCAll=currenttempWDCAll;
         oldtempWCCAll=currenttempWCCAll;
-        if predictionData.wcc_data.includes(racename) {
+        if (predictionData.wcc_data.includes(racename)) {
             currenttempWCC1 = Object.entries(
                 Object.fromEntries(
                     Object.entries(predictionData.wcc_data[racename])
